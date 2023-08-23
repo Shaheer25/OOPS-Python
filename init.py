626f5f210540e145988b1546413cd7e7b7c930e1
Shaheer25 <shaheer@logichive.in>
2023-08-23 12:05:32 +0530

    init

--- init.py
+++ init.py
@@ -1,2 +1,14 @@
-def __init__(self):
-    pass
+class Computer:
+
+    def __init__(self, processor,ram):
+        self.processor=processor
+        self.ram = ram
+
+    def config(self):
+        print(self.processor,self.ram)
+
+lenovo= Computer("i5","512")
+dell = Computer("Ryzen","1TB")
+
+lenovo.config()
+dell.config()
